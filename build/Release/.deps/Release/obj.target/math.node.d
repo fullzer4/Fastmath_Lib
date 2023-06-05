ad26cf569b730a417c90556605386713d458ef74
Gabriel Pelizzaro Pereira <gabrielpelizzaro@gmail.com>
2023-06-05 09:51:43 -0300

    Delete build directory

--- build/Release/.deps/Release/obj.target/math.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/math.node := g++ -o Release/obj.target/math.node -shared -pthread -rdynamic -m64  -Wl,-soname=math.node -Wl,--start-group Release/obj.target/math/math.o -Wl,--end-group 
